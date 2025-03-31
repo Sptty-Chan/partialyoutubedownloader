b5ba30fcfc008704693cdfd5b05b02ad389fdbfa
Fanda <89897469+Sptty-Chan@users.noreply.github.com>
2025-03-31 12:20:22 +0800

    Update ytdownloader.py

--- ytdownloader.py
+++ ytdownloader.py
@@ -3,8 +3,11 @@ import random, string, os
 
 url = input("[??]. Masukkan url youtube lalu enter: ")
 yt = YouTube(url)
-namaFile = "".join(random.choice(f"{string.ascii_lowercase}{string.ascii_uppercase}") for n in range(10))
-yt.title = namaFile
+ytmp3 = Youtube(url)
+namaFileVideo = "".join(random.choice(f"{string.ascii_lowercase}{string.ascii_uppercase}") for n in range(10))
+namaFileAudio = "".join(random.choice(f"{string.ascii_lowercase}{string.ascii_uppercase}") for n in range(10))
+yt.title = namaFileVideo
+ytmp3.title = namaFileAudio
 dataRes = {}
 for num, st in enumerate(yt.streams):
     if st.resolution and st.mime_type=="video/mp4":
@@ -16,12 +19,14 @@ for key, value in dataRes.items():
 resC = str(int(input("[??]. Pilih resolusi lalu enter: ")))
 print("[..]. Sedang mengunduh...")
 print("[..]. Tunggu sebentar, proses ini tidak memakan kuota internet")
-dataRes[resC].download(output_path="/content")
-
+fileVideo = dataRes[resC].download(output_path="/content")
+fileAudio = ytmp3.streams.filter(only_audio=True).first().download(output_path="/content")
+os.system(f"ffmpeg -i {fileVideo} -i {fileAudio} -map 0:v -map 1:a -c:v copy -c:a copy -shortest /content/output.mp4")
+os.system(f"rm -rf {fileVideo} {fileAudio}")
 print("\n[**]. Pilih bagian yang ingin didownload lalu enter")
 print("[**]. Format hh:mm:ss")
 print("[**]. Contoh: 00:03:27\n")
 start = input("[??]. Start dari (hh:mm:ss): ")
 end = input("[??]. End sampai (hh:mm:ss): ")
 
-os.system(f"ffmpeg -ss {start} -to {end} -i /content/{namaFile}.mp4 -c copy /content/output.mp4")
+os.system(f"ffmpeg -ss {start} -to {end} -i /content/output.mp4 -c copy /content/outputFinal.mp4")
